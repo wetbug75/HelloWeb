952a76817591c2c13d57f9088d209089dec1896c
Jeff Sterling <jsterling@overdrive.com>
2014-05-13 13:57:54 -0400

    Added a list of alert modes to the home page.

--- HelloWeb/Controllers/HomeController.cs
+++ HelloWeb/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Web.Mvc;
+using HelloWeb.Enumerations;
+using HelloWeb.Models;
 
 namespace HelloWeb.Controllers
 {
@@ -6,9 +8,36 @@ namespace HelloWeb.Controllers
 	{
 		public ActionResult Index()
 		{
-			ViewBag.Message = "Welcome to ASP.NET MVC Andrew!";
+			// lookup what the current alert type is:
+			var currentAlertType = AlertType.TestOff;
 
-			return View();
+			// define the model for the home page:
+			var model = new HomeModel
+				{
+					AlertModes = new []
+						{
+							new AlertMode
+								{
+									Type = AlertType.TestOff,
+									Name = "Test - OFF",
+									IsSelected = (currentAlertType == AlertType.TestOff)
+								},
+							new AlertMode
+								{
+									Type = AlertType.TestOn,
+									Name = "Test - ON",
+									IsSelected = (currentAlertType == AlertType.TestOn)
+								},
+							new AlertMode
+								{
+									Type = AlertType.TestBlink,
+									Name = "Test - BLINK",
+									IsSelected = (currentAlertType == AlertType.TestBlink)
+								}
+						}
+				};
+
+			return View(model);
 		}
 
 		public ActionResult Status()
